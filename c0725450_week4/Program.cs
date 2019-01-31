6ea9baff37beae357e8b4c93e8b61d5e3d517151
c0725450 <komalgillc3@gmail.com>
2019-01-31 14:13:20 -0500

    changes at 2:13

--- c0725450_week4/Program.cs
+++ c0725450_week4/Program.cs
@@ -4,12 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0725450_week4
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
         }
+    }
+    class Village
+    {
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string villageName;
+        public bool isAstrildeHere = false;
+
+        public Village()
+        {
+            Village.numberOfVillages++;
+        }
+
+    }
+    class Countryside
+    {
+        public Village Maple;
+        public Village Toronto;
+        public Village Ajax;
+
+        public void MapInitializer()
+        {
+            Maple = new Village();
+            Maple.villageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+
+            Toronto = new Village();
+            Toronto.villageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousVillage = Maple;
+
+            Ajax = new Village();
+            Ajax.villageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+            Ajax.isAstrildeHere = true;
+        }
+
+        public void LookForAstrilde()
+        {
+
+        }
+
     }
 }
